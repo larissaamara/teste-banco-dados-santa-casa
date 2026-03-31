74b1d43e9c7e7c04a1375aaab1128ccbf3df1a04
Larissa Amaral <souza.larissaamaral@gmail.com>
2026-03-31 15:57:35 -0300

    Add SQL script to insert sample data

---
+++ scripts/scripts/insert_data.sql
@@ -0,0 +1,9 @@
+INSERT INTO clientes VALUES (1, 'Larissa', 'larissa@email.com');
+INSERT INTO clientes VALUES (2, 'João', 'joao@email.com');
+
+INSERT INTO produtos VALUES (1, 'Notebook', 3000);
+INSERT INTO produtos VALUES (2, 'Mouse', 100);
+
+INSERT INTO vendas VALUES (1, 1, 1, 2);
+INSERT INTO vendas VALUES (2, 2, 2, 5);
+INSERT INTO vendas VALUES (3, 1, 2, 3);
