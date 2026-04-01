cc3b9c12a6b2885d75801274fc384908137de7fc
Larissa Amaral <souza.larissaamaral@gmail.com>
2026-03-31 21:21:57 -0300

    Update SQL query for sales and products

--- scripts/queries.sql
+++ scripts/queries.sql
@@ -1,7 +1,8 @@
 SELECT 
-    p.nome,
-    SUM(v.quantidade) AS total_vendido
-FROM vendas v
-JOIN produtos p ON v.id_produto = p.id_produto
-GROUP BY p.nome
+    produtos.id,
+    produtos.nome,
+    SUM(itens_venda.quantidade) total_vendido
+FROM itens_venda
+JOIN produtos ON produtos.id = itens_venda.produto_id
+GROUP BY produtos.id, produtos.nome
 ORDER BY total_vendido DESC;
