cd9e31566d20939eaa267aa799d0c9c2d5d2b98f
Larissa Amaral <souza.larissaamaral@gmail.com>
2026-03-31 21:56:13 -0300

    Fix client total in sales insertion

--- scripts/insert_data.sql
+++ scripts/insert_data.sql
@@ -14,7 +14,7 @@ INSERT INTO produtos (nome, preco, estoque) VALUES ('Headset', 300.00, 15);
 
 Criar venda
 INSERT INTO vendas (cliente_id, total) VALUES (1, 3500.00);
-INSERT INTO vendas (cliente_id, total) VALUES (2, 1020.00);
+INSERT INTO vendas (cliente_id, total) VALUES (2, 490.00);
 INSERT INTO vendas (cliente_id, total) VALUES (3, 250.00);
 INSERT INTO vendas (cliente_id, total) VALUES (4, 1320.00);
 INSERT INTO vendas (cliente_id, total) VALUES (5, 500.00);
